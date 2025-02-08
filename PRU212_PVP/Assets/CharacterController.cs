aab2a3378a3259809923e315c747cbd8aad98af9
Thang20041963 <thangndhe186090@fpt.edu.vn>
2025-02-08 14:06:02 +0700

    add normal action for character

--- PRU212_PVP/Assets/CharacterController.cs
+++ PRU212_PVP/Assets/CharacterController.cs
@@ -205,7 +205,6 @@ public abstract class CharacterController : MonoBehaviour
     {
         rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
         animator.SetBool("run", false);
-        animator.SetBool("jump", false);
     }
 
     private bool IsGrounded() => Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
